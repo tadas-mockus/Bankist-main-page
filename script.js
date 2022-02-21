c7e1e9c612b11038449aec07828e75a32c7f6373
tadas-mockus <tadas.mockus@outlook.com>
2022-02-21 15:12:58 +0200

    final project

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 ///////////////////////////////////////
 // Modal window
 
-const modalOpenAcc = document.querySelector('.modal__Open_Account');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
@@ -14,24 +14,24 @@ const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
 
-const openModalAcc = function (e) {
+const openModal = function (e) {
   e.preventDefault();
-  modalOpenAcc.classList.remove('hidden');
+  modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modalOpenAcc.classList.add('hidden');
+  modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModalAcc));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modalOpenAcc.classList.contains('hidden')) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
